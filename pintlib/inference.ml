0913974bdd96e9190affb5712a4fd4ba77630499
Loic Pauleve <pauleve@pc-pauleve>
2008-09-30 11:31:15 +0200

    MAJOR: simplify constraints API (no more Or, Empty, etc..)
    original bzr status:
    revno: 14
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Tue 2008-09-30 11:31:15 +0200

--- pintlib/inference.ml
+++ pintlib/inference.ml
@@ -5,43 +5,39 @@ type t_trace = Trace of (Spig.t_state list)
 type t_prop = float
 ;;
 
-let transition_exists (Spig.Transition name, dest) =
-	Constraint.Relation (name, Constraint.Greater 0.)
-;;
-let transition_never (Spig.Transition name, dest) =
-	Constraint.Relation (name, Constraint.Equal 0.)
+let transition_exists (Spig.Transition name, dest) = (name, Constraint.NotNull);;
+let transition_never (Spig.Transition name, dest) = (name, Constraint.Null);;
+
+let rec _always spig = function [] | [_] -> []
+	| s1::s2::q ->
+		let rset = List.filter (fun (tr,d) -> d <> s2) (Spig.next spig s1)
+		in
+		let cs = List.map transition_never rset
+		in
+		cs @ (_always spig (s2::q))
 ;;
 
 let rec exists spig = function
-	  Trace([]) | Trace([_]) -> Constraint.Empty
+	  Trace([]) | Trace([_]) -> []
 	| Trace(s1::s2::q) -> 
 		let rset = List.filter (fun (tr, d) -> d = s2) (Spig.next spig s1)
 		in
-		let cs = List.map transition_exists rset
+		assert (List.length rset = 1);
+		let cs = transition_exists (List.hd rset)
 		in
-		let c = List.fold_left Constraint.disjonction Constraint.Empty cs
-		in Constraint.And(c, exists spig (Trace (s2::q)))
+		cs::(exists spig (Trace (s2::q)))
 
 	| Stable(trace, []) -> exists spig (Trace trace)
 	| Stable(trace, h::q) ->
-		let cycle = h::q @ h::[]
+		let cycle = h::q @ [h]
 		in
-		let rec no_deviation = function [] | [_] -> Constraint.Empty
-			| s1::s2::q ->
-				let rset = List.filter (fun (tr,d) -> d <> s2) (Spig.next spig s1)
-				in
-				let cs = List.map transition_never rset
-				in
-				let c = List.fold_left Constraint.conjonction Constraint.Empty cs
-				in
-				Constraint.And(c, no_deviation (s2::q))
-		in
-		Constraint.And(exists spig (Trace (trace@cycle)), no_deviation cycle)
+		(exists spig (Trace (trace@cycle))) @ (_always spig cycle)
 ;;
 
 let rec proportion spig p = function
-	  Stable (trace, h::cycle) -> Constraint.And(proportion spig p (Trace (trace@[h])),
-	  											exists spig (Stable ([], h::cycle)))
+	  Stable (trace, h::cycle) -> 
+	  	(proportion spig p (Trace (trace@[h]))) @ 
+			(exists spig (Stable ([], h::cycle)))
 	| Stable (trace, []) -> proportion spig p (Trace trace)
 	| Trace [] | Trace [_] -> invalid_arg "trace must have at least 2 states!"
 	| Trace trace ->
@@ -87,11 +83,9 @@ let rec proportion spig p = function
 			| [t] -> deviant_exp
 			| t::q -> Constraint.Div (deviant_exp, prod_of_list (vars_of_list q))
 		in
-		let relop = Constraint.FactorEqual (factor, div)
+		let relation = Constraint.FactorEqual (factor, div)
 		and var = List.hd following
 		in 
-		let cs = Constraint.Relation (var, relop)
-		in
-		Constraint.And (exists spig (Trace trace), cs)
+		(var, relation)::exists spig (Trace trace)
 ;;
 
