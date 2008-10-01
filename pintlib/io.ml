fda0f6e01b5e64ee3d997c43f19720da24d74a49
Loic Pauleve <pauleve@pc-pauleve>
2008-10-01 15:59:43 +0200

    valution + ui module
    original bzr status:
    revno: 16
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Wed 2008-10-01 15:59:43 +0200

--- pintlib/io.ml
+++
@@ -1,8 +0,0 @@
-
-let dump_to_file filename content = 
-	let fd = open_out filename in
-	output_string fd content;
-	close_out fd
-;;
-
-
---
+++ ui.ml
@@ -0,0 +1,22 @@
+
+let show_results constraints =
+	print_endline (Constraint.string_of_constraints Spig.string_of_rname constraints);
+	(try
+		let valuation, equation = Valuation.valuation_of_constraints constraints
+		in
+			print_endline (Valuation.string_of_valuation Spig.string_of_rname valuation);
+			if not (Valuation.equation_solved equation) then
+				print_endline ("TO SOLVE : "^(Valuation.string_of_equation Spig.string_of_rname equation))
+	with Valuation.No_solution -> print_endline "NO SOLUTIONS!");
+	print_endline ""
+;;
+
+let exists spi trace =
+	print_endline (">>> exists "^(Inference.string_of_trace Spig.string_of_state trace));
+	Inference.exists spi trace
+;;
+let proportion spi p trace =
+	print_endline (">>> proportion "^(string_of_float p)^" "^(Inference.string_of_trace Spig.string_of_state trace));
+	Inference.proportion spi p trace
+;;
+
