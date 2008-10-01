0e78b2cb58e4fba4becd8cc7e180275f2d6dcdf7
Loic Pauleve <pauleve@pc-pauleve>
2008-10-01 17:18:40 +0200

    spi_of_spig
    original bzr status:
    revno: 17
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Wed 2008-10-01 17:18:40 +0200

--- pintlib/graph.mli
+++
@@ -1,6 +0,0 @@
-type ('a, 'b) t = ('a, 'b * 'a) Hashtbl.t
-val create : int -> ('a, 'b) t
-val add : ('a, 'b) t -> 'a -> 'b * 'a -> unit
-val get : ('a, 'b) t -> 'a -> ('b * 'a) list
-val iter : ('a -> ('b * 'a) -> unit) -> ('a, 'b) t -> unit
-val to_dot : ('a, 'b) t -> ('a -> string) -> ('b -> string) -> string
--- ui.ml
+++ ui.ml
@@ -1,4 +1,21 @@
 
+let exists spi trace =
+	print_endline (">>> exists "^(Inference.string_of_trace Spig.string_of_state trace));
+	Inference.exists spi trace
+;;
+
+let proportion spi p trace =
+	print_endline (">>> proportion "^(string_of_float p)^" "^(Inference.string_of_trace Spig.string_of_state trace));
+	Inference.proportion spi p trace
+;;
+
+let make_spi spig constraints default_rate init_state =
+	let valuation, equation = Valuation.valuation_of_constraints constraints
+	in 
+	assert (Valuation.equation_solved equation);
+	Spig.spi_of_spig spig valuation default_rate init_state
+;;
+
 let show_results constraints =
 	print_endline (Constraint.string_of_constraints Spig.string_of_rname constraints);
 	(try
@@ -9,14 +26,5 @@ let show_results constraints =
 				print_endline ("TO SOLVE : "^(Valuation.string_of_equation Spig.string_of_rname equation))
 	with Valuation.No_solution -> print_endline "NO SOLUTIONS!");
 	print_endline ""
-;;
-
-let exists spi trace =
-	print_endline (">>> exists "^(Inference.string_of_trace Spig.string_of_state trace));
-	Inference.exists spi trace
-;;
-let proportion spi p trace =
-	print_endline (">>> proportion "^(string_of_float p)^" "^(Inference.string_of_trace Spig.string_of_state trace));
-	Inference.proportion spi p trace
 ;;
 
