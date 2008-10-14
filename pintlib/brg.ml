f3e2568686800ff29767e1098c63104145d5508d
Loic Pauleve <pauleve@pc-pauleve>
2008-10-14 21:07:31 +0200

    new api/inference methods
    original bzr status:
    revno: 22
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Tue 2008-10-14 21:07:31 +0200

--- pintlib/brg.ml
+++ pintlib/brg.ml
@@ -10,6 +10,7 @@ let create = Graph.create;;
 let add (brg:t) = Graph.add brg;;
 let get (brg:t) = Graph.get brg;;
 let iter f (brg:t) = Graph.iter f brg;;
+let genes (brg:t) = Graph.vertices brg;;
 
 let string_of_edge (t, e) = 
 	string_of_int(t)^","^(match e with Activation -> "+" | Inhibition -> "-")
@@ -20,18 +21,22 @@ let to_dot (brg:t) = Graph.to_dot brg string_of_gene string_of_edge;;
 
 let not_epsilon = function Activation -> Inhibition | Inhibition -> Activation;;
 
+let level_max (brg:t) gene = 
+	let highest_t last ((t, epsilon), j) = max last t
+	in
+	List.fold_left highest_t 0 (get brg gene)
+;;
+
 let spig_of_brg (brg:t) = let spig = Spig.create 1 in
-	let proc_of_gene_level gene level = gene^string_of_int level
+	let proc_of_gene_level gene level = (gene, level)
 	and rate_name_of_gene_action g1 l1 g2 l2 =
 		g1^(string_of_int l1)^g2^(string_of_int l2)
 	in
 	let rec xiter func a b = if b >= a then (func b; xiter func a (b-1))
 	in
-	let highest_t last ((t, epsilon), j) = max last t
-	in
 	let apply_action i ((t,epsilon), j) =
-		let bi = List.fold_left highest_t 0 (get brg i)
-		and bj = List.fold_left highest_t 0 (get brg j)
+		let bi = level_max brg i
+		and bj = level_max brg j
 		and epsilon' e = if e < t then not_epsilon epsilon else epsilon
 		in
 		let op e = match epsilon' e with Activation -> 1 | Inhibition -> -1
@@ -50,7 +55,7 @@ let spig_of_brg (brg:t) = let spig = Spig.create 1 in
 				Spig.add spig pjf (Spig.Call rname, pjf'))
 			else
 				if e <> f' then
-					Spig.add spig pie (Spig.Delay ("d"^pie), pjf')
+					Spig.add spig pie (Spig.Delay ("d"^(Spig.string_of_pi_proc pie)), pjf')
 		in
 		if i <> j then
 			let minlevel e = match epsilon' e with Activation -> 0
@@ -64,5 +69,43 @@ let spig_of_brg (brg:t) = let spig = Spig.create 1 in
 	in
 	iter apply_action brg;
 	spig
+;;
+
+let constraints_of_k (brg:t) (spig:Spig.t) x rx k =
+	let levels y =
+		match List.filter (fun ((t,e), x') -> x = x') (get brg y) with
+			  [] -> Util.range 0 (level_max brg y)
+			| [((t,e),x')] ->
+				let min_l, max_l = (match List.mem y rx, e with
+					  true, Activation | false, Inhibition -> (t, level_max brg y)
+					| true, Inhibition | false, Activation -> (0, t-1)
+				) in
+				Util.range min_l max_l
+			| _ -> failwith "Invalid BRG (more than one action)"
+	in
+	let all_procs = List.map
+		(fun y -> List.map (fun l -> y,l) (levels y))
+		(genes brg)
+	in
+	let states = Util.cross_list all_procs
+	in
+	let level_from_proc (p,l) = l
+	in
+	let proc_from_state state = 
+		match List.filter (fun (y,_) -> x = y) state with
+			  [] -> raise Not_found
+			| [p] -> p
+			| _ -> failwith "Invalid state (duplicated gene type)"
+	in
+	let pred orig dest = 
+		let lx = level_from_proc (proc_from_state orig)
+		and lx' = level_from_proc (proc_from_state dest)
+		in
+		lx <= k && lx' < lx || lx >= k && lx' > lx
+	in
+	Util.list_uniq (List.flatten (List.map
+		(Inference.never_transition_matching_from_state spig pred)
+		states))
+;;
 
 
--- ui.ml
+++ ui.ml
@@ -1,4 +1,32 @@
 
+let build_brg brg_spec =
+	let brg = Brg.create 0 
+	in
+	List.iter (fun (a, t, e, b) -> Brg.add brg a ((t,e), b)) brg_spec;
+	brg
+;;
+
+let show_deleted_channels chans =
+	let chans = Util.list_uniq chans
+	in
+	print_endline ("deleted "^String.concat " " (List.map Spig.string_of_transition chans));
+	chans
+;;
+
+let spi_stable_state spi state =
+	print_endline (">>> stable "^Spig.string_of_state state);
+	let chans = Spig.stable_state spi state
+	in
+	show_deleted_channels chans
+;;
+
+let spi_stable spi state substs =
+	print_endline (">>> stable "^Dynamic.string_of_substs Spig.string_of_state state substs);
+	let chans = Spig.stable spi state substs
+	in
+	show_deleted_channels chans
+;;
+
 let exists spi trace =
 	print_endline (">>> exists "^(Inference.string_of_trace Spig.string_of_state trace));
 	Inference.exists spi trace
@@ -32,5 +60,15 @@ let show_results constraints =
 			)
 	with Valuation.No_solution -> print_endline "NO SOLUTIONS!");
 	print_endline ""
+;;
+
+let spig_of_k brg spig (d, r, k) =
+    let ck = Brg.constraints_of_k brg spig d r k
+	in
+	print_string ("K"^d^"{"^(String.concat ";" r)^"} = "^(string_of_int k)^" => ");
+	print_constraints ck;
+	let spig2, cs = Spig.apply_constraints spig ck
+	in
+	spig2
 ;;
 
