01157ef38347344f9a7a90429beb877189994d27
Loic Pauleve <pauleve@pc-pauleve>
2008-11-21 15:41:18 +0100

    K -> decisions; implementation of free_transition
    original bzr status:
    revno: 30
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Fri 2008-11-21 15:41:18 +0100

--- pintlib/decision.ml
+++ pintlib/decision.ml
@@ -99,11 +99,34 @@ let drivers_e decisions (((m,l),a),s) =
 	List.filter (fun (m',i,a') -> m = m' && a = a' && state_match_dyninfo i s) decisions
 ;;
 
+let involved_metaproc_dyninfo =
+	let rec folder genes = function
+		  L (a,x) -> a::genes
+		| Neg b -> folder genes b
+	in
+	List.fold_left folder []
+;;
+
+let involved_procs_dyninfo =
+	let rec folder procs = function
+		  L p -> p::procs
+		| Neg b -> folder procs b
+	in
+	List.fold_left folder []
+;;
+
 (*
  * decision
  *)
 let string_of_decision (m, i, a) =
 	"\\delta_{"^m^"}("^(string_of_dyninfo i)^")"^string_of_action a
+;;
+
+let select metaproc = List.filter (fun (m,_,_) -> m = metaproc)
+;;
+
+let transitions_matching_decision (m,i,a) =
+	List.filter (fun (_,s) -> state_match_dyninfo i s)
 ;;
 
 let apply_decision state (m,i,a) =
@@ -158,101 +181,54 @@ let dynamic_of_decisions decisions states =
 	dyn
 ;;
 
-let solve_conflict (m,i,a) pctxs cctxs =
-	let solve_conflict_1 pctx =
-		List.map (fun cctx -> Util.list_sub pctx cctx) cctxs
+let rec free_transition (m,i,a) (t,s) constraints =
+	let im = involved_procs_dyninfo i
 	in
-	let sols = List.flatten (List.map solve_conflict_1 pctxs)
+	let remove_root s = Util.list_sub s im
 	in
-	let compare_sol s1 s2 = compare (List.length s1) (List.length s2)
+	let r = remove_root s
 	in
-	let sols = Util.list_uniq (List.map Util.list_uniq (Util.cross_list sols))
+	let cr = List.flatten (List.map 
+		(fun (_,s) -> Util.list_intersection r s) constraints)
 	in
-	let rec decisions_of_sol = function [] -> []
-		| p::q -> (m,(L p)::i,a)::(m,(Neg (L p))::i,a)::decisions_of_sol q
+	let p = Util.list_sub r cr
 	in
-	let sols = List.sort compare_sol sols
-	in
-	decisions_of_sol (List.hd sols)
+	if List.length p > 0 then (m,(L (List.hd p))::i,a)
+	else (
+		let i = (L (List.hd cr))::i
+		in
+		let constraints = List.filter (fun (_,s) ->
+			state_match_dyninfo i s) constraints
+		in
+		free_transition (m,i,a) (t,s) constraints
+	)
 ;;
 
 let solve states decisions (constraints,properties) =
+	let rec free_properties decisions constraints cds = function
+		  [] -> decisions
+		| t::q -> let ds = drivers_e decisions t in
+			if Util.subset cds ds then
+				let d = List.hd ds
+				in
+				let nd = free_transition d t (transitions_matching_decision d constraints)
+				in
+				free_properties (nd::decisions) constraints cds q
+			else free_properties decisions constraints cds q
+	in
+
 	let extend tl =
 		List.flatten (List.map (t_extend states) tl)
 	in
 	let constraints = extend constraints
 	and properties = extend properties
 	in
-
-	let cdsm = List.map (fun t -> t, drivers_e decisions t) constraints
-	and pdsm = List.map (fun t -> t, drivers_e decisions t) properties
+	let cds = Util.list_uniq (List.flatten (List.map
+		(fun t -> drivers_e decisions t) constraints))
 	in
 
-	let ds_from_map m = Util.list_uniq (List.flatten (snd (List.split m)))
-	and h_from_map m pred =
-		let h = Hashtbl.create (List.length m)
-		in
-		let register_assoc (t,ds) =
-			List.iter (fun d -> if pred d then Hashtbl.add h d (snd t)) ds
-		in
-		List.iter register_assoc m;
-		h
+	let decisions = free_properties decisions constraints cds properties
 	in
-
-	let cds = ds_from_map cdsm
-	in
-
-	(* 1. filter free properties *)
-	let p_not_free (t, ds) = Util.subset cds ds
-	in
-	let pdsm = List.filter p_not_free pdsm
-	in
-
-	(* 2. disable free decisions *)
-	let pds = ds_from_map pdsm
-	in
-	let fd = Util.list_sub cds pds
-	in
-	let decisions = (Util.list_sub decisions fd) @
-			List.map (fun (m,i,_) -> (m,i,Dis)) fd
-
-	(* 3. resolve conflicts *)
-	and cdsh = h_from_map cdsm (fun d -> not (List.mem d fd))
-	and pdsh = h_from_map pdsm (fun d -> true)
-	in
-	let dconflicts = List.map 
-		(fun d -> d,(Hashtbl.find_all pdsh d),(Hashtbl.find_all cdsh d))
-		pds
-	in
-	let dresols = List.map
-		(fun (d, pctxs, cctxs) -> d, (solve_conflict d pctxs cctxs))
-		dconflicts
-	in
-
-	(** DEBUG **)
-	let string_of_conflict_resol (conflict, resol) =
-		let string_of_conflict (d, pctxs, cctxs) =
-			"Conflict: "^(string_of_decision d)^" with "^
-			(string_of_context_list pctxs)^" and "^
-			(string_of_context_list cctxs)^"."
-		and string_of_resol (d, ds') =
-			"replace "^(string_of_decision d)^" by "^
-			String.concat ", " (List.map string_of_decision ds')
-		in
-		(string_of_conflict conflict)^"\n\tsolution: "^
-		string_of_resol resol
-	in
-	let crs = List.map string_of_conflict_resol (List.combine dconflicts dresols)
-	in
-	print_endline (String.concat "\n" (List.map string_of_decision decisions));
-	print_endline (String.concat "\n" crs);
-	(** FIN DEBUG *)
-
-	(*
-	let pds = snd (List.split pdsm)
-	in
-	let all_solving_ds = List.map Util.list_uniq (Util.cross_list pds)
-	in*)
-	decisions
+	Util.list_sub decisions cds
 ;;
 
