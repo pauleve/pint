30828cc7abec2602bec0b91629a45a3ff83ae71a
Loic Pauleve <pauleve@pc-pauleve>
2009-03-03 14:46:54 +0100

    wake process faster
    original bzr status:
    revno: 63
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Tue 2009-03-03 14:46:54 +0100

--- phlib/ph.ml
+++ phlib/ph.ml
@@ -107,7 +107,7 @@ let spim_of_ph2 (ps,hits) init_state properties =
 		(String.concat "\n" (List.map string_of_channel channels))
 	and body = "let "^
 		String.concat "\nand " (List.map string_of_piproc piprocs)^
-		"\n\nlet w() = delay@0.1; w()"
+		"\n\nlet w() = delay@0.5; w()"
 
 	and directives = String.concat "\n" [
 		"directive sample "^Util.string_of_float0 (float_of_string (List.assoc "sample" properties));
