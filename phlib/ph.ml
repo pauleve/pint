7caf5426c56786425cb7c15e12b1477cc81e61c9
Loic Pauleve <pauleve@pc-pauleve>
2009-03-23 19:33:15 +0100

    docs ph-language
    original bzr status:
    revno: 64
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Mon 2009-03-23 19:33:15 +0100

--- phlib/ph.ml
+++ phlib/ph.ml
@@ -4,7 +4,7 @@ open Ph_types;;
 type piproc_arg_action = ArgReset | ArgUpdate of (string * string) list;;
 
 (* spim_of_ph with stochasticity_absorption *)
-let spim_of_ph2 (ps,hits) init_state properties =
+let spim_of_ph (ps,hits) init_state properties =
 	let chanl_of_process p = "l_"^p
 	and p_level (p,l) = p^"("^string_of_int l^")"
 	and pi_name_level (p,l) = p^string_of_int l
@@ -125,76 +125,4 @@ let spim_of_ph2 (ps,hits) init_state properties =
 							(List.combine ps init_state))) ^ "| w())\n"
 	in
 	directives ^ "\n\n" ^ defs ^ "\n\n" ^ body ^ "\n\n" ^ run
-
-let spim_of_ph (ps,hits) init_state properties =
-	let name_of_process (p,l) = p^string_of_int l^"()"
-	in
-	let register_metaproc piprocs (p,l) =
-		piprocs @ List.map (fun l -> (name_of_process (p,l),[])) (Util.range 0 l)
-	in
-	let piprocs = List.fold_left register_metaproc [] ps
-	in
-	let add_pichoice piprocs piproc pi =
-		let pis = List.assoc piproc piprocs
-		in
-		(piproc,pi::pis)::List.remove_assoc piproc piprocs
-	in
-	let register_hit p2 ((p1,r),l) (piprocs,channels,counter) =
-		let cid = "hit"^string_of_int counter
-		and p2' = (fst p2,l)
-		in
-		let np1 = name_of_process p1
-		and np2 = name_of_process p2
-		and np2' = name_of_process p2'
-		in
-		let piprocs = 
-			if p1 = p2 then (
-				(* delay *)
-				let pi = if r = RateInf then np2' else ("delay@"^cid^";"^np2')
-				in
-				add_pichoice piprocs np2 pi
-			) else (
-				let pi1 = "!"^cid^";"^np1
-				and pi2 = "?"^cid^";"^np2'
-				in
-				let piprocs = add_pichoice piprocs np1 pi1
-				in
-				add_pichoice piprocs np2 pi2
-			)
-		in
-		let c = (cid, r, p2 <> p1)
-		in
-		(piprocs,c::channels,counter+1)
-	in
-	let piprocs,channels,counter = Hashtbl.fold register_hit hits (piprocs,[],0)
-	in
-	let string_of_channel (cid, rate, ischan) = match ischan with
-		  true -> "new "^cid^(match rate with 
-		  				Rate f -> "@"^Util.string_of_float0 f
-						| RateInf -> "")^":chan"
-		| false -> (match rate with
-						Rate f -> "val "^cid^"="^Util.string_of_float0 f
-						| RateInf -> "")
-	and string_of_piproc (piproc, choices) =
-		piproc ^ " = " ^ match choices with
-				  [] -> "!dead"
-				| [pi] -> pi
-				| pis -> "do "^String.concat " or " pis
-	in
-	
-	let defs = "new dead:chan\n" ^ 
-		(String.concat "\n" (List.map string_of_channel channels))
-	and body = "let "^
-		String.concat "\nand " (List.map string_of_piproc piprocs)
-
-	and directives = String.concat "\n" [
-		"directive sample "^Util.string_of_float0 (float_of_string (List.assoc "sample" properties));
-		"directive plot "^String.concat ";" (fst (List.split piprocs))]
-
-	and run = "run ("^(String.concat " | " 
-					(List.map (fun ((n,_),l) -> name_of_process (n,l))
-						(List.combine ps init_state))) ^ ")\n"
-	in
-	directives ^ "\n\n" ^ defs ^ "\n\n" ^ body ^ "\n\n" ^ run
-;;
 
