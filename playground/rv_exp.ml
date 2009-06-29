cc40ffe46f2a86c8134b0d2fbf061dde2985a015
Loic Pauleve <pauleve@pc-pauleve>
2009-06-29 19:06:40 +0200

    update
    original bzr status:
    revno: 101
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Mon 2009-06-29 19:06:40 +0200

--- playground/rv_exp.ml
+++ playground/rv_exp.ml
@@ -4,17 +4,22 @@ let exponential r =
 ;;
 
 let _ =
+	Random.self_init();
 	let rate = float_of_string (Sys.argv.(1))
 	and count = int_of_string (Sys.argv.(2))
 	in
-	let rec iter acc n = if n > 0 then (
+	let rec iter acc n = if n <= count then (
 		let acc = acc +. exponential rate
 		in
-		print_endline (string_of_float acc);
-		iter acc (n-1)
+		if n mod 5 = 0 then (
+			print_endline ("\\draw<3> ("^string_of_float acc^",0) -- ("^string_of_float acc^",1);");
+			print_endline ("\\draw<4->[hl] ("^string_of_float acc^",0) -- ("^string_of_float acc^",1);");
+		) else
+			print_endline ("\\draw<3> ("^string_of_float acc^",0) -- ("^string_of_float acc^",1);");
+		iter acc (n+1)
 	)
 	in
-	iter 0.0 count
+	iter 0.0 1
 ;;
 
 
