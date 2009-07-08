0b92c5e5315e780d90a1c0d1e64f9d4f9666d47b
Panard <panard@backzone.net>
2009-07-08 21:31:09 +0200

    qerlang: use boolean for lower flag
    original bzr status:
    revno: 107
    committer: Panard <panard@backzone.net>
    branch nick: pint
    timestamp: Wed 2009-07-08 21:31:09 +0200

--- commonlib/param.ml
+++ commonlib/param.ml
@@ -3,7 +3,7 @@ let firing_interval alpha r sa =
 	let rate = r*.float_of_int sa
 	and alpha = alpha/.2.
 	in
-	R.qerlang alpha sa rate 1, R.qerlang alpha sa rate 0
+	R.qerlang alpha sa rate true, R.qerlang alpha sa rate false
 ;;
 
 
