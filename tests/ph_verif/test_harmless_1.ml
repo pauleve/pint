844408a572ac2dd77812b5ef9028d290ff497e7f
Panard <panard@backzone.net>
2009-12-12 01:54:19 +0100

    harmless implementation 1
    original bzr status:
    revno: 122
    committer: Panard <panard@backzone.net>
    branch nick: pint
    timestamp: Sat 2009-12-12 01:54:19 +0100

--- tests/ph_verif/test_harmless_1.ml
+++ tests/ph_verif/test_harmless_1.ml
@@ -1,13 +1,15 @@
 
 open Ui;;
 
-let test_model phname (z, lz') =
-	print_endline ("#test_model "^phname);
+let test_model phname arg =
+	print_endline ("#<<<<<< test_model "^phname);
 	let ph = ph_load (phname^".ph")
 	in
-	let ret = Ph_verif.harmless ph (z,lz')
+	let ret = Ph_verif.harmless ph arg
 	in
-	ret
+	print_endline ("#>>>>>> ["^phname^"] harmless("^
+		Ph_verif.string_of_sortdomain arg^
+		") = " ^ Ph_verif.BS.string_of_dnf ret)
 in
 
 test_model "simple" ("z", [1])
