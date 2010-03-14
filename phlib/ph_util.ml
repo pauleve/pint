e4e5e40cbdbdf1ddb389100e575febfd9eb09fdc
Loïc Paulevé <loic.pauleve@gmail.com>
2010-03-14 23:05:36 +0100

    correctly initialize initial state
    original bzr status:
    revno: 143
    committer: Loïc Paulevé <loic.pauleve@gmail.com>
    branch nick: pint
    timestamp: Sun 2010-03-14 23:05:36 +0100

--- phlib/ph_util.ml
+++ phlib/ph_util.ml
@@ -1,13 +1,22 @@
 
 open Ph_types;;
 
+let fill_state =
+	let folder state (a,_) =
+		if not(SMap.mem a state) then
+			SMap.add a 0 state
+		else state
+	in
+	List.fold_left folder
+;;
+
 let parse channel_in =
 	let lexbuf = Lexing.from_channel channel_in
 	in
 	try 
 		let ph, init_state = Ph_parser.main Ph_lexer.lexer lexbuf
 		in
-		ph, init_state
+		ph, fill_state init_state (fst ph)
 	with Parsing.Parse_error ->
 		let show_position pos =
 			  "Line " ^ string_of_int pos.Lexing.pos_lnum ^
