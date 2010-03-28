aa2a6a55f2ae6e8f046bb85ee6675720ac6e7efc
Panard <panard@backzone.net>
2010-03-28 19:09:53 +0200

    process reachability: use of process equivalences
    original bzr status:
    revno: 156
    committer: Panard <panard@backzone.net>
    branch nick: pint
    timestamp: Sun 2010-03-28 19:09:53 +0200

--- phreach.ml
+++ phreach.ml
@@ -1,16 +1,17 @@
 
 open Debug;;
-
 open Ph_types;;
 
 let phname = Sys.argv.(1)
-let zl = Sys.argv.(2), int_of_string (Sys.argv.(3));;
-
-let ph, state = Ui.ph_load2 phname;;
-let nb_actions = Ph_op.ph_count_actions ph;;
-
-dbg (phname^": "^(string_of_int nb_actions)^" actions");;
-dbg ("# testing reachability of "^string_of_process zl^" from state "^string_of_state state);;
+in
+let zl = Sys.argv.(2), int_of_string (Sys.argv.(3))
+in
+let ph, state = Ui.ph_load2 phname
+in
+let nb_actions = Ph_op.ph_count_actions ph
+in
+dbg ("# "^phname^": "^(string_of_int nb_actions)^" actions");
+dbg ("# testing reachability of "^string_of_process zl^" from state "^string_of_state state);
 
 (*
 let hdepend, _ = Ph_verif.process_reachability_prepare ph zl state
@@ -23,6 +24,6 @@ in
 let decision = Ph_reach.process_reachability env zl state
 in
 match decision with
-  true -> (dbg "# SUCCESS"; exit 0)
+true -> (dbg "# SUCCESS"; exit 0)
 | false -> (dbg "# FAILURE"; exit 1)
 
