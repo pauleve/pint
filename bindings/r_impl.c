e76280fdc7fc982fd99d7c1e47a02e7bbe68b295
Loïc Paulevé <loic.pauleve@gmail.com>
2010-05-03 11:56:50 +0200

    rerlang binding
    original bzr status:
    revno: 173
    committer: Loïc Paulevé <loic.pauleve@gmail.com>
    branch nick: pint
    timestamp: Mon 2010-05-03 11:56:50 +0200

--- bindings/r_impl.c
+++ bindings/r_impl.c
@@ -6,4 +6,7 @@
 double qerlang( double area, int shape, double rate, int lower ) {
 	return qgamma(area, (double)shape, 1/rate, lower, 0);
 }
+double rerlang( int shape, double rate ) {
+	return rgamma((double) shape, 1/rate);
+}
 
