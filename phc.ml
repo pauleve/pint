d348462ac0a97a274f5a1a15f6fae08702740028
Loic Pauleve <pauleve@pc-pauleve>
2010-05-18 17:54:27 +0200

    ph2prism: use constant for sa
    original bzr status:
    revno: 180
    committer: Loic Pauleve <pauleve@pc-pauleve>
    branch nick: pint
    timestamp: Tue 2010-05-18 17:54:27 +0200

--- phc.ml
+++ phc.ml
@@ -4,40 +4,49 @@
 
 open Ph_translator;;
 
+let languages = ["dump"; "spim"; "prism"; "prism_mdp"; "romeo"; "tina"; "biocham"];;
 
-let _ =
-	let language, filename, output = match Array.length Sys.argv with
-		4 -> Sys.argv.(1), Sys.argv.(2), Sys.argv.(3)
-		| _ -> failwith "Usage: phc <-spim|-prism|...> <source.ph> <output>"
-	in
-	let opts = {
-		alpha = 0.05;
-		round_fi = Param.round_fi_ex
-	}
-	in
-	let languages = [
-		("dump", dump_of_ph);
-		("spim", spim_of_ph);
-		("prism", prism_of_ph);
-		("prism_mdp", prism_mdp_of_ph);
-		("romeo", romeo_of_ph opts);
-		("tina", tina_of_ph);
-		("biocham", biocham_of_ph);
+let opt_language = ref "dump"
+and opt_input = ref ""
+and opt_output = ref ""
+in
+let cmdopts = Ui.common_cmdopts @ [
+		("-l", Arg.Symbol (languages, (fun l -> opt_language := l)), "\tOutput language");
+		("-i", Arg.Set_string opt_input, "\tInput filename");
+		("-o", Arg.Set_string opt_output, "\tOutput filename");
 	]
-	in
-	let translator = List.assoc (Str.string_after language 1) languages
-	in
+and usage_msg = "phc"
+in
+let anon_fun _ = (Arg.usage cmdopts usage_msg; raise Exit)
+in
+Arg.parse cmdopts anon_fun usage_msg;
+let opts = {
+	alpha = 0.05;
+	round_fi = Param.round_fi_ex
+}
+in
+let languages = [
+	("dump", dump_of_ph);
+	("spim", spim_of_ph);
+	("prism", prism_of_ph);
+	("prism_mdp", prism_mdp_of_ph);
+	("romeo", romeo_of_ph opts);
+	("tina", tina_of_ph);
+	("biocham", biocham_of_ph);
+]
+in
+let translator = List.assoc !opt_language languages
+in
 
-	let channel_in = open_in filename
-	in
-	let ph, init_state = Ph_util.parse channel_in
-	in
-	let data = translator ph init_state
-	in
-	let fd_out = match output with "-" -> stdout
-					| _ -> open_out output
-	in
-	output_string fd_out data;
-	close_out fd_out
-;;
+let channel_in = if !opt_input = "" then stdin else open_in !opt_input
+in
+let ph, init_state = Ph_util.parse channel_in
+in
+close_in channel_in;
+let data = translator ph init_state
+in
+let channel_out = if !opt_output = "" then stdout else open_out !opt_output
+in
+output_string channel_out data;
+close_out channel_out
 
