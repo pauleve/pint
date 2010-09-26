0ec63573f84379ab598afafb3973f757205e5f96
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-09-26 23:29:07 +0200

    add kappa translation
    original bzr status:
    revno: 189
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Sun 2010-09-26 23:29:07 +0200

--- phc.ml
+++ phc.ml
@@ -40,7 +40,7 @@ knowledge of the CeCILL license and that you accept its terms.
 
 open Ph_translator;;
 
-let languages = ["dump"; "spim"; "prism"; "prism_mdp"; "romeo"; "tina"; "biocham"];;
+let languages = ["dump"; "spim"; "prism"; "prism_mdp"; "romeo"; "tina"; "biocham"; "kappa"];;
 
 let opt_language = ref "dump"
 and opt_input = ref ""
@@ -69,6 +69,7 @@ let languages = [
 	("romeo", romeo_of_ph opts);
 	("tina", tina_of_ph);
 	("biocham", biocham_of_ph);
+	("kappa", kappa_of_ph);
 ]
 in
 let translator = List.assoc !opt_language languages
