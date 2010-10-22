8231466cc41bfd7a9ff513e3d523aa2d3fbe67c0
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-10-22 19:38:22 +0200

    ph-exec: use common input options
    original bzr status:
    revno: 216
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Fri 2010-10-22 19:38:22 +0200

--- phexec.ml
+++ phexec.ml
@@ -44,18 +44,18 @@ R.set_seed (Random.bits ()) (Random.bits ());;
 
 let opt_args = ref []
 in
-let cmdopts = Ui.common_cmdopts
-and usage_msg = "ph-exec [opts] <model.ph> <duration> <outputdir>"
+let cmdopts = Ui.common_cmdopts @ Ui.input_cmdopts
+and usage_msg = "ph-exec [opts] <duration> <outputdir>"
 and anon_fun arg = opt_args := !opt_args@[arg]
 in
 Arg.parse cmdopts anon_fun usage_msg;
-let phname, duration, outputdir = match !opt_args with
-	  [phname; duration; outputdir] -> phname, float_of_string duration, outputdir
+let duration, outputdir = match !opt_args with
+	  [d;o] -> float_of_string d, o
 	| _ -> (Arg.usage cmdopts usage_msg; raise Exit)
 in
 
 
-let ph, state = Ui.ph_load2 phname
+let ph, state = Ph_util.parse !Ui.opt_channel_in
 in
 
 (** plot **)
