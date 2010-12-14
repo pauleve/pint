3820628753afda0cb8aafd8a4d2a48e4c524ea9f
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-12-14 02:35:57 +0100

    APIDOC
    original bzr status:
    revno: 231
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Tue 2010-12-14 02:35:57 +0100

--- pintlib/debug.mli
+++ pintlib/debug.mli
@@ -1,3 +1,11 @@
+(** Debugging control. *)
+
+(** Set to [true] if debug messages should be displayed. *)
 val dodebug : bool ref
-val dbg_noendl : string -> unit
+
+(** [dbg msg] prints [msg^"\n"] to standard error channel. *)
 val dbg : string -> unit
+
+(** [dbg_noendl] prints [msg] to standard error channel. *)
+val dbg_noendl : string -> unit
+
