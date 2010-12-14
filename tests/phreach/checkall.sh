a1560766c1c98a125b949a35bc2a1173ad0bab15
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-12-14 10:38:54 +0100

    tests/phreach/checkall.sh: in case of failure, copy the changed output
    original bzr status:
    revno: 233
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Tue 2010-12-14 10:38:54 +0100

--- tests/phreach/checkall.sh
+++ tests/phreach/checkall.sh
@@ -9,15 +9,17 @@ for x in *.cmd; do
 
 	if diff -q $n.stdout $sout > /dev/null; then
 		echo -n " [stdout ok]"
+		rm -f $sout
 	else
 		echo -n " [STDOUT KO]"
+		mv -b $sout $n.stdout.err
 	fi
 	if diff -q $n.stderr $serr > /dev/null; then
 		echo -n " [stderr ok]"
+		rm -f $serr
 	else
 		echo -n " [STDERR KO]"
+		mv -b $serr $n.stderr.err
 	fi
 	echo
-
-	rm -f $serr $sout
 done
