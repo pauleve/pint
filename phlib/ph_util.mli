b237383f80c5d09947bca7f193807fa28c743435
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-12-17 09:53:58 +0100

    move ph_* methods from Ui to Ph_util
    original bzr status:
    revno: 250
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Fri 2010-12-17 09:53:58 +0100

--- phlib/ph_util.mli
+++ phlib/ph_util.mli
@@ -2,3 +2,9 @@ val fill_state : Ph_types.state -> Ph_types.process list -> Ph_types.state
 val opt_initial_procs : Ph_types.process list ref
 val parse : in_channel -> Ph_types.ph * Ph_types.state
 val matching : Ph_types.ph -> (Ph_types.action -> bool) -> Ph_types.action list
+
+val count_states : Ph_types.ph -> Big_int.big_int
+val count_sorts : Ph_types.ph -> int
+val count_processes : Ph_types.ph -> int
+val count_actions : Ph_types.ph -> int
+
--- ui.ml
+++ ui.ml
@@ -34,31 +34,6 @@ same conditions as regards security.
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 *)
-(****
-	SHORTCUTS
- ****)
-
-open Big_int;;
-
-let ph_load filename = fst (Ph_util.parse (open_in filename));;
-let ph_load2 filename = Ph_util.parse (open_in filename);;
-
-let ph_stable_states = Ph_static.stable_states;;
-
-let ph_count_states (ps,hits) = 
-	let counter c (_,la) = mult_int_big_int (la+1) c
-	in
-	List.fold_left counter unit_big_int ps
-;;
-let ph_count_sorts (ps, _) = List.length ps
-;;
-let ph_count_processes (ps, _) =
-	let c acc (_,l) = acc + l + 1
-	in
-	List.fold_left c 0 ps
-;;
-let ph_count_actions (_, hits) = Hashtbl.length hits;;
-
 
 let setup_opt_initial_procs opt =
 	try 
