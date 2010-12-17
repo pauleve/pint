a8fd35a0242d3b2b7a945150377c7700e4dd6cd2
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2010-12-17 10:30:36 +0100

    Ph_util API cleanup and doc
    original bzr status:
    revno: 252
    committer: Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
    branch nick: pint
    timestamp: Fri 2010-12-17 10:30:36 +0100

--- phlib/ph_util.mli
+++ phlib/ph_util.mli
@@ -1,11 +1,18 @@
 (** Process Hitting helpers. *)
 
+(** Reference to the process list that should be present in the initial state. *)
 val opt_initial_procs : Ph_types.process list ref
-val parse : in_channel -> Ph_types.ph * Ph_types.state
-val matching : Ph_types.ph -> (Ph_types.action -> bool) -> Ph_types.action list
 
+(** Parse a Process Hitting and its initial state from given input channel.
+	Overwrite the initial state with the processes referenced by [opt_initial_procs]. *)
+val parse : in_channel -> Ph_types.ph * Ph_types.state
+
+(** Returns the total number of states of the given Process Hitting. *)
 val count_states : Ph_types.ph -> Big_int.big_int
+(** Returns the  number of sorts in the given Process Hitting. *)
 val count_sorts : Ph_types.ph -> int
+(** Returns the  number of processes in the given Process Hitting. *)
 val count_processes : Ph_types.ph -> int
+(** Returns the  number of actions in the given Process Hitting. *)
 val count_actions : Ph_types.ph -> int
 
