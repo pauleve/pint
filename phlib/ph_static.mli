b4f65ce60f83386d4e1c4b4b78be31adc3c8822c
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2014-04-14 13:50:28 +0200

    cleanup

--- phlib/ph_static.mli
+++
@@ -1,8 +0,0 @@
-(** Static properties of Process Hitting *)
-
-(** returns the set of dynamics fix points of the given Process Hitting *)
-val stable_states : Ph_types.ph -> Ph_types.process list list
-
-val resolve_cooperativities : Ph_types.ph -> Ph_types.process list -> 
-			(Ph_types.process * Ph_types.PSet.t list) list
-
