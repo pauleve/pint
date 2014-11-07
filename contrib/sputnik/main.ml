3c3fde4c6dde5e4f413fc5fe30de335149a620a3
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2014-11-07 20:52:33 +0100

    prepare for sputnik interface

--- contrib/sputnik/main.ml
+++ contrib/sputnik/main.ml
@@ -1,7 +1,39 @@
 
+open PintTypes
+
 let test () =
 	print_endline ("Pint - Sputnik");
 	true
 
+let dump_ph outf ph ctx =
+	let data = Ph_translator.dump_of_ph ph ctx
+	in
+	let cout = open_out outf
+	in
+	output_string cout data;
+	close_out cout
 
+let init_stack =
+	Stack.create
+
+let push_ph stack ph =
+	let state = ph, Ph_instance.copy ()
+	in
+	Stack.push state stack
+
+let top_ph stack =
+	let ph, instance = Stack.top stack
+	in
+	Ph_instance.restore instance;
+	ph
+
+let pop_ph stack =
+	ignore(Stack.pop stack)
+
+let worth_glc ph ctx goal =
+	let ctx = Ph_cooperativity.coherent_ctx !Ph_instance.cooperativities ctx
+	in
+	let env = Ph_reach.init_env ph ctx [goal]
+	in
+	Ph_reach.worth_glc env
 
