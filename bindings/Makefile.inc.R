43f8545cc9d09420df840d360bb25155913c98c7
Loïc Paulevé <loic.pauleve@lri.fr>
2015-01-26 17:59:59 +0100

    opam support++

--- bindings/Makefile.inc.R
+++ bindings/Makefile.inc.R
@@ -8,7 +8,9 @@ CLIBS := m Rmath
 BINDINGS_SOURCES := $(SOURCES:%=bindings/%)
 export BINDINGS_SOURCES
 
+ifdef HAS_OPAM
 OCAML_DEFAULT_DIRS += $(OCAML_LIBDIR)camlidl
 export OCAML_DEFAULT_DIRS
+endif
 
 # vi:set syntax=make:
