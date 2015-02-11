59ee8cf1c4fb1ac561b6e7eb1a21e869635f378f
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2015-02-11 14:45:36 +0100

    remove ph-reach --cutsets (already in pint-reach)

--- phreach.ml
+++ phreach.ml
@@ -41,25 +41,14 @@ open Ph_types;;
 open Ph_glc;;
 open Ph_reach;;
 
-let opt_method = ref "static"
-and opt_args = ref []
-and opt_cutsets_n = ref 0
-and opt_cutsets_n_coop = ref false
-and opt_cutsets_n_reduce = ref true
+let opt_args = ref []
 and opt_extract_graph = ref ""
 and opt_graph = ref "verbose"
 and opt_coop_priority = ref false
 in
 let cmdopts = Ui.common_cmdopts @ Ui.input_cmdopts @ [
-		("--method", Arg.Symbol (["static";"test"],
-				(fun x -> opt_method := x)), "\tMethod");
 		("--coop-priority", Arg.Set opt_coop_priority, 
 									"\tAssume hits on cooperative sorts of higher priority");
-		("--cutsets", Arg.Set_int opt_cutsets_n, "n\tCompute cutsets with given maximum size");
-		("--cutsets-include-coop", Arg.Set opt_cutsets_n_coop, 
-			"\tConsider cooperativities in cutsets (default: false)");
-		("--no-cutsets-reduce", Arg.Clear opt_cutsets_n_reduce, 
-			"\tDisable static reduction of causality abstract structre");
 		("--output-glc", Arg.Set_string opt_extract_graph, 
 				"<graph.dot>\tExport the Graph of Local Causality (GLC)");
 		("--glc", Arg.Symbol (["verbose";"trimmed";"cutsets";"saturated";"first_ua"],
@@ -75,10 +64,9 @@ in
 let pl = Ui.proclist_from_stringlist !opt_args
 in
 
-let do_cutsets = !opt_cutsets_n > 0
-and do_extract_graph = !opt_extract_graph <> ""
+let do_extract_graph = !opt_extract_graph <> ""
 in
-let do_reach = not (do_cutsets || do_extract_graph)
+let do_reach = not do_extract_graph
 in
 
 let ph, ctx = Ph_util.parse !Ui.opt_channel_in
@@ -87,96 +75,6 @@ in
 let env = Ph_reach.init_env ph ctx pl
 in
 
-(if do_cutsets then
-	let is_sort_cooperative a = 
-		(try String.sub a 0 1 = "_" with Invalid_argument _ -> false) 
-			|| SMap.mem a !Ph_instance.cooperativities
-	in
-	let ignore_proc ai = if !opt_cutsets_n_coop then false else is_sort_cooperative (fst ai)
-	in
-		
-	(* compute graph *)
-    let gA = new glc oa_glc_setup env.ctx env.pl env.concrete (get_Sols env)
-    in  
-    gA#set_auto_conts false;
-    gA#build;
-    let gA = bot_trimmed_cwA env gA
-    in  
-	let gA = if !opt_cutsets_n_reduce then
-		let gA = cleanup_gA_for_cutsets gA
-		in
-		top_trimmed_cwA env gA;
-		gA else gA
-	in
-	prerr_endline ("#nodes = "^string_of_int gA#count_nodes);
-	prerr_endline ("#procs = "^string_of_int gA#count_procs);
-	prerr_endline ("#objs = "^string_of_int gA#count_objs);
-
-    let (d_nkp, index_proc) = cutsets gA !opt_cutsets_n ignore_proc gA#leafs
-	in
-
-	(* TODO
-	let find_coops pss =
-		let process_ps ps coops =
-			let ps_coops = PSet.filter (fun (a,_) -> is_sort_cooperative a) ps
-			in
-			PSet.union ps_coops coops
-		in
-		PSSet.fold process_ps pss PSet.empty
-	in
-	*)
-		(* TODO 
-			let coops = find_coops pss
-			in
-			let coops = Ph_static.resolve_cooperativities ph (PSet.elements coops)
-			in
-			let string_of_rcoops psl =
-				let string_of_rcoop =
-					string_of_set ~lbracket:"<" ~rbracket:">" ~delim:"," string_of_proc PSet.elements
-				in
-				String.concat ";" (List.map string_of_rcoop psl)
-			in
-		*)
-
-	let resolve_ps = 
-		List.map (Hashtbl.find index_proc)
-	in
-	let string_of_ai ai =
-		(*if is_sort_cooperative (fst ai) then TODO
-			string_of_rcoops (List.assoc ai coops)
-		else*)
-			string_of_proc ai
-	in
-	let print_ps ps =
-		let s = String.concat ";" (List.map string_of_ai ps)
-		in
-		print_endline s
-	in
-	let handle_proc ai =
-		try
-			let pss = fst (Hashtbl.find d_nkp (NodeProc ai))
-			in
-			let n = PSSet.cardinal pss
-			in
-			prerr_endline ("# "^string_of_int n^" key process(es) for "^string_of_proc ai^":");
-			let elts = PSSet.elements pss
-			in
-			let elts = List.map resolve_ps elts
-			in
-			let elts = List.map (List.sort compare) elts
-			in
-			let elts = List.sort (fun a b ->
-					let c = compare (List.length a) (List.length b)
-					in
-					if c <> 0 then c else compare a b) elts
-			in
-			List.iter print_ps elts
-
-		with Not_found ->
-			print_endline (string_of_node (NodeProc ai)^" is not reachable.");
-	in
-	List.iter handle_proc pl
-);
 (if do_extract_graph then 
 	let get_Sols = Ph_bounce_seq.get_aBS env.ph env.bs_cache
 	in
@@ -243,14 +141,11 @@ in
 );
 (if do_reach then
 	(* run the approximations *)
-	let decision = 
-	match !opt_method with
-		| "static" ->
-			if !opt_coop_priority then
-				Ph_reach.coop_priority_reachability env
-			else
-				Ph_reach.local_reachability env
-		| _ -> failwith "Unknown method."
+	let decision =
+		if !opt_coop_priority then
+			Ph_reach.coop_priority_reachability env
+		else
+			Ph_reach.local_reachability env
 	in
 	print_endline (string_of_ternary decision)
 )
