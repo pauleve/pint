9f12244f803a0ba51f57362614e38645428898d9
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2016-12-15 16:12:41 +0100

    ipython: print version at init

--- interfaces/ipython/pint/__init__.py
+++ interfaces/ipython/pint/__init__.py
@@ -1,6 +1,16 @@
 
+import subprocess
+
 from .cfg import *
 from .model import *
 from .ui import *
 from .tools import *
+
+def hello_ipython():
+    version = subprocess.check_output(["pint-config", "version"]).decode()
+    info("You are using Pint version %s" % version)
+
+
+if IN_IPYTHON:
+    hello_ipython()
 
