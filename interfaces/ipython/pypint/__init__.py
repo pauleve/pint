d9d8d81e6ea0462f4f3c91151e9d1e2dbbeb0158
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2017-06-30 20:04:51 +0200

    pypint: check required pint binaries

--- interfaces/ipython/pypint/__init__.py
+++ interfaces/ipython/pypint/__init__.py
@@ -7,8 +7,10 @@ When loaded, the `pypint` module will perform the follow tasks:
   :py:func:`.ipython_install`.
 """
 __version__ = "1.2.4"
+__pint_required__ = "2017-06-30"
 
 import os
+import shutil
 import subprocess
 import sys
 
@@ -24,7 +26,25 @@ from .utils import *
 
 __SETUP_DONE = False
 
+__PINT_VERSION__ = None
+
+def check_pint():
+    if shutil.which("pint-config") is None:
+        raise Exception("Pint binaries should be installed separately. " + \
+            "See https://loicpauleve.name/pint/doc/#Binaries")
+
+    global __PINT_VERSION__
+    version = subprocess.check_output(["pint-config", "version"]).decode()
+    __PINT_VERSION__ = version.strip()
+
+    if __pint_required__ > __PINT_VERSION__:
+        raise Exception("Pint >= {0} is required. Please upgrade Pint binaries. " \
+                .format(__pint_required__) +  \
+            "See https://loicpauleve.name/pint/doc/#Binaries" )
+
+
 def setup_environ():
+    check_pint()
     global __SETUP_DONE
     share_path = subprocess.check_output(["pint-config", "share-path"]).decode()
     bin_path = os.path.join(share_path.strip(), "bin")
@@ -32,9 +52,7 @@ def setup_environ():
     __SETUP_DONE = True
 
 def hello_ipython():
-    version = subprocess.check_output(["pint-config", "version"]).decode()
-    version = version.strip()
-    info("You are using Pint version %s and pypint %s" % (version,__version__))
+    info("You are using Pint version %s and pypint %s" % (__PINT_VERSION__,__version__))
 
 
 if not __SETUP_DONE:
