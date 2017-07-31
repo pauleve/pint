4103b958d22a76d168f94e7044ac015f2a3355ca
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2017-07-31 14:07:11 +0200

    new test for pint-reach with sync transitions (issue #8)

--- tests/TestPintReach.ml
+++ tests/TestPintReach.ml
@@ -2,7 +2,6 @@
 open OUnit2
 
 open PintTypes
-open Ph_types
 
 open TestCommon
 
@@ -28,26 +27,19 @@ let test_exe ?length:(length=OUnitTest.Short)
 	test_case ~length test
 
 let tests =
-	"TestPintReach" >::: [
-		"Intermediary1" >:
-			test_exe "models/interm1.an"
-						["d=2"]
-						Inconc;
-		"CoopPrioMetazoan" >:
-			test_exe "models/metazoan-3prio-flattening.an"
-						~ctx:"a=1, f=1, c=0"
-						["a=0"]
-						True;
-		"CoopPrioTCR1" >:
-			test_exe "models/tcrsig94-TCS.an"
-						~ctx:"cd45=1, cd28=1, tcrlig=1"
-						["ap1=1"]
-						True;
-		(*
-		"CoopPrioInconc1" >:
-			test_exe "models/1-02.an"
-						["bp"; "1"; "z"; "1"]
-						Inconc; *)
-	]
+"TestPintReach" >::: [
+    "SyncSimple02" >:
+        test_exe "models/sync_simple02.an" ["c=1"] Inconc;
+    "Intermediary1" >:
+        test_exe "models/interm1.an" ["d=2"] Inconc;
+    "Metazoan" >:
+        test_exe "models/metazoan-3prio-flattening.an"
+                    ~ctx:"a=1, f=1, c=0"
+                    ["a=0"] True;
+    "TCR1" >:
+        test_exe "models/tcrsig94-TCS.an"
+                    ~ctx:"cd45=1, cd28=1, tcrlig=1"
+                    ["ap1=1"] True;
+]
 
 
