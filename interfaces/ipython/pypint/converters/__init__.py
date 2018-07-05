68b51b1e9c49cfb5a1f07a52498faff441c3bf41
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-07-05 15:49:18 +0200

    minibn upgraded to colomoto

--- interfaces/ipython/pypint/converters/__init__.py
+++ interfaces/ipython/pypint/converters/__init__.py
@@ -2,6 +2,7 @@
 from .biocham import import_biocham
 from .cadbiom import import_cadbiom
 from .sbgnpd import import_sbgnpd
+from .minibn import import_minibn
 
 CONVERTERS = {
     "biocham": import_biocham,
--- interfaces/ipython/pypint/converters/minibn.py
+++ interfaces/ipython/pypint/converters/minibn.py
@@ -1,56 +1,32 @@
-try:
-    import boolean
-except ImportError:
-    print("ERROR: Please install boolean.py package (pip3 install boolean.py)")
-    raise
+from colomoto_jupyter.sessionfiles import new_output_file
+from pypint.converters.lib.boolean_utils import BoolToAN
+from pypint.converters.lib.export_utils import pint_protect
 
+def import_minibn(f):
+    ba = f.ba
 
-class BooleanNetwork(dict):
-    def __init__(self, *args, **kwargs):
-        dict.__init__(self, *args, **kwargs)
-        self.ba = boolean.BooleanAlgebra()
-    def vars(self, names):
-        return self.ba.symbols(*names)
+    def ls_of_lit(lit):
+        if isinstance(lit, ba.NOT):
+            return (lit.args[0].obj, 0)
+        else:
+            return (lit.obj, 1)
 
-    def to_pint(self):
-        from colomoto_jupyter.sessionfiles import new_output_file
-        from pypint.converters.lib.boolean_utils import BoolToAN
-        from pypint.converters.lib.export_utils import pint_protect
-        ba = self.ba
+    anfile = new_output_file(ext="an")
+    with open(anfile, "w") as outf:
+        def out(data):
+            print(data, file=outf)
+        b2a = BoolToAN(ba, ls_of_lit, out)
 
-        def ls_of_lit(lit):
-            if isinstance(lit, ba.NOT):
-                return (lit.args[0].obj, 0)
-            else:
-                return (lit.obj, 1)
+        for a in sorted(f.keys()):
+            out("{} [0, 1]".format(pint_protect(a)))
 
-        anfile = new_output_file(ext="an")
-        with open(anfile, "w") as f:
+        for a, fa in sorted(f.items()):
+            sa = f.vars(a)[0]
+            expr_up = fa.subs({sa: ba.FALSE}).simplify()
+            expr_down = (~fa).subs({sa: ba.TRUE}).simplify()
+            b2a.make_transitions([(a, 0, 1)], expr_up)
+            b2a.make_transitions([(a, 1, 0)], expr_down)
 
-            def out(data):
-                print(data, file=f)
-            b2a = BoolToAN(ba, ls_of_lit, out)
-
-            for a in sorted(self.keys()):
-                out("{} [0, 1]".format(pint_protect(a)))
-
-            for a, fa in sorted(self.items()):
-                [sa] = self.vars([a])
-                expr_up = fa.subs({sa: ba.FALSE}).simplify()
-                expr_down = (~fa).subs({sa: ba.TRUE}).simplify()
-                b2a.make_transitions([(a, 0, 1)], expr_up)
-                b2a.make_transitions([(a, 1, 0)], expr_down)
-
-        import pypint
-        return pypint.load(anfile)
-
-if __name__ == "__main__":
-    bn = BooleanNetwork()
-    a, b, c = bn.vars(["a", "b", "c"])
-    bn["a"] = a
-    bn["b"] = ~b
-    bn["c"] = a | ~b
-    an = bn.to_pint()
-    print(an.source())
-
+    import pypint
+    return pypint.load(anfile)
 
