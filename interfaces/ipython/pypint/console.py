905f0199250830c4306d578acb93357ec51c977a
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-11-06 14:33:46 +0100

    pint-import: add support for specific arguments

--- interfaces/ipython/pypint/console.py
+++ interfaces/ipython/pypint/console.py
@@ -12,9 +12,17 @@ def import_model():
     parser = ArgumentParser(prog=sys.argv[0])
     parser.add_argument("input")
     parser.add_argument("--output", "-o", type=str, default=None)
-    args = parser.parse_args()
 
-    an = load(args.input)
+    sbgnpd_opts = parser.add_argument_group("SBGN-PD import")
+    sbgnpd_opts.add_argument("--fancy-names", action="store_true", default=False,
+                help="Use EPN labels as automata names")
+
+    args = parser.parse_args()
+    opts = vars(args).copy()
+    del opts["input"]
+    del opts["output"]
+
+    an = load(args.input, **opts)
 
     if args.output:
         an.save_as(args.output)
