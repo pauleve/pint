118272642b984a9a5125e245a3755b276cebff00
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-12-21 14:36:27 +0100

    pypint v1.5.1

--- interfaces/ipython/pypint/__init__.py
+++ interfaces/ipython/pypint/__init__.py
@@ -6,7 +6,7 @@ When loaded, the `pypint` module will perform the follow tasks:
 * if in IPython, displays the version of Pint binaries, and executes
   :py:func:`.ipython_install`.
 """
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 import os
 import shutil
